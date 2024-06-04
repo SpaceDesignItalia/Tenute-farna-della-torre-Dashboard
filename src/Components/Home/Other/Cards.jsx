16f1996ecef194b2ad452e62565eb4226affe225
Andrea Braia <andrix.braia@gmail.com>
2024-06-04 21:41:47 +0200

    EditProduct

--- src/Components/Home/Other/Cards.jsx
+++ src/Components/Home/Other/Cards.jsx
@@ -1,11 +1,41 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { API_URL } from "../../../API/API";
 
 export default function Cards() {
+  const [statsData, setStatsData] = useState({
+    usersNumber: 0,
+    customersNumber: 0,
+    ordersNumber: 0,
+  });
+
+  useEffect(() => {
+    axios.get(API_URL + "/Analytic/GetUsersNumber").then((res) => {
+      setStatsData((prevData) => ({
+        ...prevData,
+        usersNumber: res.data.analytic[0].CustomersNumber,
+      }));
+    });
+    axios.get(API_URL + "/Analytic/GetCustomersNumber").then((res) => {
+      setStatsData((prevData) => ({
+        ...prevData,
+        customersNumber: res.data,
+      }));
+    });
+    axios.get(API_URL + "/Analytic/GetOrdersNumber").then((res) => {
+      setStatsData((prevData) => ({
+        ...prevData,
+        ordersNumber: res.data,
+      }));
+    });
+  }, []);
+
   const stats = [
-    { name: "Clienti registrati", stat: "300" },
-    { name: "Ordini", stat: "15" },
-    { name: "Ricavo", stat: "3500 $" },
+    { name: "Clienti registrati", stat: statsData.usersNumber },
+    { name: "Clienti", stat: statsData.customersNumber },
+    { name: "Ordini", stat: statsData.ordersNumber },
   ];
+
   function classNames(...classes) {
     return classes.filter(Boolean).join(" ");
   }
